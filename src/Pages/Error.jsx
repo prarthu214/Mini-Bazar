0b0b6a19f9f858e2d4dbe8f292474dfae9cda67c
prarthu214 <prarthanagowda7879@gmail.com>
2025-03-01 12:03:35 +0530

    first commit

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -8,7 +8,7 @@ export default function Error() {
       <img src="https://img.freepik.com/free-vector/error-404-concept-illustration_114360-1811.
       // jpg?t=st=1740803951~exp=1740807551~hmac=5970bea8bdb9dc91219d53ddf19c988530b6b6e5c9eb17b13dd71f6a59cea6bd&w=1380"
       alt="" />
-      <h>hii</h>
+      <h>hi am</h>
     </div>
   );
   
